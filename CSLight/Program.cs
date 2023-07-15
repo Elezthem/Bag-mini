f1e053ae86c7a594b397afb2a58a002272dbdc5d
Elezthem <97851451+Elezthem@users.noreply.github.com>
2023-07-15 22:07:59 +0300

    Add files via upload

---
+++ CSLight/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] bag = new int[1];
+            bag[0] = 12;
+            int[] tempBag = new int[bag.Length + 1];
+            for (int i = 0; i < bag.Length; i++)
+            {
+                tempBag[i] = bag[i];
+                Console.WriteLine(bag[i] + " Первый");
+            }
+
+            Console.WriteLine();
+
+            tempBag[tempBag.Length - 1] = 56;
+
+            bag = tempBag;
+
+            for (int i = 0; i < bag.Length; i++)
+            {
+                Console.WriteLine(bag[i] + " Второй");
+            }
+        }
+    }
+}
